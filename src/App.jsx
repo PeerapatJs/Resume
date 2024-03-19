b3b8c9bc727f0be2affaf293ba79ef40dbab9e68
Peerapath Klaipant <peerapat.klai@bumail.net>
2024-03-19 21:22:04 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,121 @@
+// App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+      <div className='circle'>
+        <img src="MicrosoftTeams-image (8).jpg" alt="Description of your image" />
+      </div>
+        <h1>PEERAPAT
+          <div className='lastname'>
+            <h1>KLIAPANT</h1>
+          </div>
+        </h1>
+      </header>
+      <section className="main">
+        <div className="left">
+          <div className='left h2'>
+          <h2>EXPERIENCE</h2>
+          </div>
+        <div className='tag'>
+          <ul>
+            <p>Automatic alcohol injection machine (Mini Project)</p>
+            <p>2022</p>
+            <p>Automatic fan 4 levels (Mini Project)</p>
+            <p>2022</p>
+          </ul>
+          </div>
+          <div className='left h2'>
+          <h2>EDUCATION</h2>
+          </div>
+          <div className='tag'>
+          <ul>
+            <p>Senior Hight School: Pakchong School</p>
+            <p>Program: Art-Computer 2017-2019</p>
+            <p>Bachelor's degree: Bangkok University</p>
+            <p>Department: Computer and Robotics Engineering 2020-2024</p>
+          </ul>
+          </div>
+          <div className='left h2'>
+          <h2>SKILLS</h2>
+          </div>
+          <div className='tag'>
+          <ul>
+            <li>HTML & CSS</li>
+            <li>Python3</li>
+            <li>Arduino</li>
+            <li>JavaScript</li>
+            <li>SQL</li>
+          </ul>
+          </div>
+          <div className='left h2'>
+          <h2>TOOLS</h2>
+          </div>
+          <div className='tag'>
+          <ul>
+            <li>Visual Studio Code</li>
+            <li>FreeCAD</li>
+            <li>Cisco Packet Tracer,Circuit Wizard</li>
+            <li>Arduino IDE</li>
+            <li>Circiut Wizard</li>
+            <li>Flowgorithm</li>
+            <li>Dev-C++</li>
+            <li>IDLE (Python)</li>
+          </ul>
+          </div>
+        </div>
+        <div className="right">
+          <div className='right h2'>
+            <ul>
+            <h2>SUMMARY</h2>
+            </ul>
+          </div>
+          <div className='tag'>
+          <ul>
+            <p>Name:Mr.Peerapat Klaipant</p>
+            <p>Age:21</p>
+            <p>Gender:Men</p>
+            <p>Birthday:18 November 2002</p>
+            <p>093-0021969</p>
+            <p>pure09401@gmail.com</p>
+            <p>ID: pure09401gmail.com</p>
+          </ul>
+          </div>
+          <div className='right h2'>
+            <ul>
+            <h2>LANGUAGE</h2>
+            </ul>
+          </div>
+          <div className='tag'>
+          <ul>
+            <li>English</li>
+            <li>Thai</li>
+          </ul>
+          </div>
+          <div className='right h2'>
+            <ul>
+            <h2>EXPERTISE</h2>
+            </ul>
+          </div>
+          <div className='tag'>
+          <ul>
+            <p>Write code for developed web
+          control hardware applications
+          for clients and basic coding
+          using
+          Arduino,C,C++,Python3,Java,Jav
+          aScript and a little bit other
+          programming languages.</p>
+          </ul>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
+
